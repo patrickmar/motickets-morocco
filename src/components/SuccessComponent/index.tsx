51a2d99f2184da15af4377999f0006b8e2921b40
sundayuchegbu <sundayuchegbu10@gmail.com>
2024-12-10 13:31:50 +0100

    commit the payment changes

--- src/components/SuccessComponent/index.tsx
+++ src/components/SuccessComponent/index.tsx
@@ -6,21 +6,21 @@ import {
   FormEventHandler,
   useEffect,
   useState,
-} from 'react';
-import axios from 'axios';
-import PhoneInput from 'react-phone-number-input';
-import 'react-phone-number-input/style.css';
-import { CountryCode, E164Number } from 'libphonenumber-js';
-import ContentWrapper from '../../components/ContentWrapper';
+} from "react";
+import axios from "axios";
+import PhoneInput from "react-phone-number-input";
+import "react-phone-number-input/style.css";
+import { CountryCode, E164Number } from "libphonenumber-js";
+import ContentWrapper from "../../components/ContentWrapper";
 
-import { NumericFormat } from 'react-number-format';
-import { Link, useNavigate } from 'react-router-dom';
-import { getCurrency, getCurrencyCode } from '../../utils/functions';
-import StripeCheckout from 'react-stripe-checkout';
-import usePost from '../../hooks/usePost';
-import './styles.scss';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { NumericFormat } from "react-number-format";
+import { Link, useNavigate } from "react-router-dom";
+import { getCurrency, getCurrencyCode } from "../../utils/functions";
+import StripeCheckout from "react-stripe-checkout";
+import usePost from "../../hooks/usePost";
+import "./styles.scss";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 type Props = {
   tickets: Array<any>;
@@ -44,19 +44,18 @@ interface IBoolean {
 }
 
 const SuccessComponent = (props: Props) => {
- 
   const { ticketData, stripeData, tickets, data } = props;
   const defaultCountryCode = process.env.REACT_APP_COUNTRYCODE;
   const taxPercent = Number(process.env.REACT_APP_TAXPERCENT);
   const baseUrl = process.env.REACT_APP_BASEURL;
-console.log(data);
+  console.log(data);
   const [tick, setTick] = useState(tickets);
   const [stripe, setStripe] = useState(stripeData);
   const [userData, setUserData] = useState(data);
   const [tickData, setTickData] = useState(ticketData);
   const [validatePay, setValidatePay] = useState(false);
   const [loading, setLoading] = useState(true);
-  
+
   const currency = data && getCurrency(data.data);
   const navigate = useNavigate();
 
@@ -99,21 +98,15 @@ console.log(data);
   //   currency: currencycode
   // });
 
-  
-
   const resetState = () => {
     setTick({});
     setStripe({});
     setUserData({});
     setTickData({});
-   
   };
 
   useEffect(() => {
-  
     const MakeRequest = async () => {
-
-      
       try {
         // console.log(userData);
         // console.log(stripe);
@@ -121,39 +114,32 @@ console.log(data);
         //  console.log(tick);
         const res = await axios
           .post(`${baseUrl}/dispense/internationalticket`, {
-            
             userdata: userData,
             stripeData: stripe,
             ticketData: tickData,
             myCart: tick,
           })
           .then((res: any) => {
-
-            
             // setLoading(false);
             // setData(res?.name === "AxiosError" ? null : res);
             setTimeout(() => {
               setLoading(false);
               resetState();
-             
-                if (res.data.error === false) {
-       
-        setValidatePay(true);
-           
-        }
+
+              if (res.data.error === false) {
+                setValidatePay(true);
+              }
               //setData(res?.name === "AxiosError" ? null : res);
             }, 5000 * 1);
           });
 
         //toast(res.data.error);
-      
-       
       } catch (error) {
         console.log(error);
       }
     };
 
-     MakeRequest();
+    MakeRequest();
   }, []);
 
   return (
@@ -161,141 +147,157 @@ console.log(data);
       {!loading ? (
         <div
           style={{
-            height: '100vh',
+            height: "100vh",
 
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            justifyContent: 'center',
-            color: 'white',
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            color: "white",
           }}
         >
-          {!validatePay
-            ? (  
-              <>
-              <span  style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            color: 'white',
-          }}>The payment is invalid! Please contact admin.</span>
-            <button
-              onClick={() => navigate('/')}
-              style={{ padding: 10, marginTop: 20, alignItems: 'center',
-              justifyContent: 'center',
-              color: 'white', }}
-              className='flex w-full items-center justify-center rounded-md border border-transparent bg-red-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-red-700'>
-              
-              Go to Homepage
-            </button>
+          {!validatePay ? (
+            <>
+              <span
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  color: "white",
+                }}
+              >
+                Le paiement est invalide ! Veuillez contacter l'administrateur.{" "}
+              </span>
+              <button
+                onClick={() => navigate("/")}
+                style={{
+                  padding: 10,
+                  marginTop: 20,
+                  alignItems: "center",
+                  justifyContent: "center",
+                  color: "white",
+                }}
+                className="flex w-full items-center justify-center rounded-md border border-transparent bg-red-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-red-700"
+              >
+                Aller à la page d'accueil
+              </button>
             </>
-            )
-            : (   
-            
+          ) : (
             <div>
-               <span style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            color: 'white',
-          }}>The tickets have been purchased successfully.</span>
-          <button
-            onClick={() => navigate('/')}
-            style={{  marginTop: 20, display: 'flex', alignItems: 'center',
-            justifyContent: 'center',
-            color: 'white', }}
-          className='flex w-full items-center justify-center rounded-md border border-transparent bg-red-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-red-700'>
-            Go to Homepage
-          </button>
-          <div
-            // style={{ width: '700px' }}
-            className="mt-4 rounded-lg border border-gray-200 bg-white shadow-sm md:w-[700px]"
-          >
-            <div className="aby border-t border-gray-200 px-4 py-6 sm:px-6">
-              {tickets.map((item, i) => (
-                <div className="flex items-center justify-between" key={i}>
-                  <dt className="text-base text-customBlack">{`${item?.qty} * ${item?.name}`}</dt>
-                  <dd className="text-base font-medium text-customBlack">
-                    <NumericFormat
-                      value={Number(item.price * item.qty).toFixed(2)}
-                      displayType={'text'}
-                      thousandSeparator={true}
-                      prefix={`${currency}`}
-                    />
-                  </dd>
+              <span
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  color: "black",
+                }}
+              >
+                Les billets ont été achetés avec succès.
+              </span>
+              <button
+                onClick={() => navigate("/")}
+                style={{
+                  marginTop: 20,
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  color: "white",
+                }}
+                className="flex w-full items-center justify-center rounded-md border border-transparent bg-red-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-red-700"
+              >
+                Aller à la page d'accueil
+              </button>
+              <div
+                // style={{ width: '700px' }}
+                className="mt-4 rounded-lg border border-gray-200 bg-white shadow-sm md:w-[700px]"
+              >
+                <div className="aby border-t border-gray-200 px-4 py-6 sm:px-6">
+                  {tickets.map((item, i) => (
+                    <div className="flex items-center justify-between" key={i}>
+                      <dt className="text-base text-customBlack">{`${item?.qty} * ${item?.name}`}</dt>
+                      <dd className="text-base font-medium text-customBlack">
+                        <NumericFormat
+                          value={Number(item.price * item.qty).toFixed(2)}
+                          displayType={"text"}
+                          thousandSeparator={true}
+                          prefix={`${currency}`}
+                        />
+                      </dd>
+                    </div>
+                  ))}
+
+                  <div className="flex items-center justify-between">
+                    <dt className="text-base text-red-600 ">Sous-total</dt>
+                    <dd className="text-base font-medium text-red-600">
+                      <NumericFormat
+                        value={Number(data.subTotal).toFixed(2)}
+                        displayType={"text"}
+                        thousandSeparator={true}
+                        prefix={`${currency}`}
+                      />
+                    </dd>
+                  </div>
+                  <div className="flex items-center justify-between">
+                    <dt className="text-base text-customBlack">
+                      Frais de réservation
+                    </dt>
+                    <dd className="text-base font-medium text-customBlack">
+                      <NumericFormat
+                        value={Number(data.totalbookingFee).toFixed(2)}
+                        displayType={"text"}
+                        thousandSeparator={true}
+                        prefix={`${currency}`}
+                      />
+                    </dd>
+                  </div>
+                  <div className="flex items-center justify-between">
+                    <dt className="text-base text-customBlack">
+                      TVA
+                      <span className="ml-2 rounded-lg bg-gray-200 px-2 py-1 text-xs tracking-wide text-gray-600">
+                        {taxPercent}%
+                      </span>
+                    </dt>
+                    <dd className="text-base font-medium text-customBlack">
+                      <NumericFormat
+                        value={Number(data.vat).toFixed(2)}
+                        displayType={"text"}
+                        thousandSeparator={true}
+                        prefix={`${currency}`}
+                      />
+                    </dd>
+                  </div>
+
+                  <div className="flex items-center justify-between border-t border-gray-200 pt-6">
+                    <dt className="text-lg text-red-600 font-bold">Total</dt>
+                    <dd className="text-base font-bold text-red-600">
+                      <NumericFormat
+                        value={Number(data.totalAmount).toFixed(2)}
+                        displayType={"text"}
+                        thousandSeparator={true}
+                        prefix={`${currency}`}
+                      />
+                    </dd>
+                  </div>
                 </div>
-              ))}
-
-              <div className="flex items-center justify-between">
-                <dt className="text-base text-red-600 ">Subtotal</dt>
-                <dd className="text-base font-medium text-red-600">
-                  <NumericFormat
-                    value={Number(data.subTotal).toFixed(2)}
-                    displayType={'text'}
-                    thousandSeparator={true}
-                    prefix={`${currency}`}
-                  />
-                </dd>
-              </div>
-              <div className="flex items-center justify-between">
-                <dt className="text-base text-customBlack">
-                  Booking Fee
-                  
-                </dt>
-                <dd className="text-base font-medium text-customBlack">
-                  <NumericFormat
-                    value={Number(data.totalbookingFee).toFixed(2)}
-                    displayType={'text'}
-                    thousandSeparator={true}
-                    prefix={`${currency}`}
-                  />
-                </dd>
-              </div>
-              <div className="flex items-center justify-between">
-                <dt className="text-base text-customBlack">
-                 VAT
-                  <span className="ml-2 rounded-lg bg-gray-200 px-2 py-1 text-xs tracking-wide text-gray-600">
-                    {taxPercent}%
-                  </span>
-                </dt>
-                <dd className="text-base font-medium text-customBlack">
-                  <NumericFormat
-                    value={Number(data.vat).toFixed(2)}
-                    displayType={'text'}
-                    thousandSeparator={true}
-                    prefix={`${currency}`}
-                  />
-                </dd>
-              </div>
-
-              <div className="flex items-center justify-between border-t border-gray-200 pt-6">
-                <dt className="text-lg text-red-600 font-bold">Total</dt>
-                <dd className="text-base font-bold text-red-600">
-                  <NumericFormat
-                    value={Number(data.totalAmount).toFixed(2)}
-                    displayType={'text'}
-                    thousandSeparator={true}
-                    prefix={`${currency}`}
-                  />
-                </dd>
               </div>
             </div>
-          </div>
-          </div>
           )}
         </div>
-     ) : (
+      ) : (
         <div className="detailsBannerSkeleton">
           <ContentWrapper>
-          
             <div className="left skeleton"></div>
             <div className="right">
-            <span style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            color: 'white',
-          }}>Loading...do not refresh.<br></br></span>
+              <span
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  color: "white",
+                }}
+              >
+                Chargement en cours... ne pas actualiser.<br></br>
+              </span>
               {[1, 2, 3, 4, 5, 6, 7].map((item, i) => (
                 <div key={i} className="row skeleton"></div>
               ))}
