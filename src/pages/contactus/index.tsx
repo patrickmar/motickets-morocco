781a2310f1cc1cb72738ff325085890b7bed0f40
sundayuchegbu <sundayuchegbu10@gmail.com>
2025-04-03 11:43:43 +0100

    Added morocco address

--- src/pages/contactus/index.tsx
+++ src/pages/contactus/index.tsx
@@ -254,76 +254,148 @@ const ContactUs = () => {
                 </dd>
               </div>
               <div className="flex gap-x-4 ">
-                <dt className="flex-none">
-                  <span className="sr-only">Adresse</span>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    xmlnsXlink="http://www.w3.org/1999/xlink"
-                    version="1.1"
-                    width="11"
-                    height="12"
-                    viewBox="0 0 256 256"
-                    xmlSpace="preserve"
-                    className="h-11 w-12 text-white bg-[#25aae1] p-4 rounded-full"
-                  >
-                    <defs></defs>
-                    <g
-                      style={{
-                        stroke: "none",
-                        strokeWidth: "0",
-                        strokeDasharray: "none",
-                        strokeLinecap: "butt",
-                        strokeLinejoin: "miter",
-                        strokeMiterlimit: "10",
-                        fill: "none",
-                        fillRule: "nonzero",
-                        opacity: "1",
-                      }}
-                      transform="translate(1.4065934065934016 1.4065934065934016) scale(2.81 2.81)"
+                <div className="flex">
+                  <dt className="flex-none">
+                    <span className="sr-only">Adresse </span>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      xmlnsXlink="http://www.w3.org/1999/xlink"
+                      version="1.1"
+                      width="11"
+                      height="12"
+                      viewBox="0 0 256 256"
+                      xmlSpace="preserve"
+                      className="h-11 w-12 text-white bg-[#25aae1] p-4 rounded-full"
                     >
-                      <path
-                        d="M 45 90 c -1.415 0 -2.725 -0.748 -3.444 -1.966 l -4.385 -7.417 C 28.167 65.396 19.664 51.02 16.759 45.189 c -2.112 -4.331 -3.175 -8.955 -3.175 -13.773 C 13.584 14.093 27.677 0 45 0 c 17.323 0 31.416 14.093 31.416 31.416 c 0 4.815 -1.063 9.438 -3.157 13.741 c -0.025 0.052 -0.053 0.104 -0.08 0.155 c -2.961 5.909 -11.41 20.193 -20.353 35.309 l -4.382 7.413 C 47.725 89.252 46.415 90 45 90 z"
+                      <defs></defs>
+                      <g
                         style={{
                           stroke: "none",
-                          strokeWidth: "1",
+                          strokeWidth: "0",
                           strokeDasharray: "none",
                           strokeLinecap: "butt",
                           strokeLinejoin: "miter",
                           strokeMiterlimit: "10",
-                          fill: "rgb(255,255,255)", // White fill color
+                          fill: "none",
                           fillRule: "nonzero",
                           opacity: "1",
                         }}
-                        transform=" matrix(1 0 0 1 0 0) "
-                        strokeLinecap="round"
-                      />
-                      <path
-                        d="M 45 45.678 c -8.474 0 -15.369 -6.894 -15.369 -15.368 S 36.526 14.941 45 14.941 c 8.474 0 15.368 6.895 15.368 15.369 S 53.474 45.678 45 45.678 z"
+                        transform="translate(1.4065934065934016 1.4065934065934016) scale(2.81 2.81)"
+                      >
+                        <path
+                          d="M 45 90 c -1.415 0 -2.725 -0.748 -3.444 -1.966 l -4.385 -7.417 C 28.167 65.396 19.664 51.02 16.759 45.189 c -2.112 -4.331 -3.175 -8.955 -3.175 -13.773 C 13.584 14.093 27.677 0 45 0 c 17.323 0 31.416 14.093 31.416 31.416 c 0 4.815 -1.063 9.438 -3.157 13.741 c -0.025 0.052 -0.053 0.104 -0.08 0.155 c -2.961 5.909 -11.41 20.193 -20.353 35.309 l -4.382 7.413 C 47.725 89.252 46.415 90 45 90 z"
+                          style={{
+                            stroke: "none",
+                            strokeWidth: "1",
+                            strokeDasharray: "none",
+                            strokeLinecap: "butt",
+                            strokeLinejoin: "miter",
+                            strokeMiterlimit: "10",
+                            fill: "rgb(255,255,255)", // White fill color
+                            fillRule: "nonzero",
+                            opacity: "1",
+                          }}
+                          transform=" matrix(1 0 0 1 0 0) "
+                          strokeLinecap="round"
+                        />
+                        <path
+                          d="M 45 45.678 c -8.474 0 -15.369 -6.894 -15.369 -15.368 S 36.526 14.941 45 14.941 c 8.474 0 15.368 6.895 15.368 15.369 S 53.474 45.678 45 45.678 z"
+                          style={{
+                            stroke: "none",
+                            strokeWidth: "1",
+                            strokeDasharray: "none",
+                            strokeLinecap: "butt",
+                            strokeLinejoin: "miter",
+                            strokeMiterlimit: "10",
+                            fill: "rgb(255,255,255)", // White fill color
+                            fillRule: "nonzero",
+                            opacity: "1",
+                          }}
+                          transform=" matrix(1 0 0 1 0 0) "
+                          strokeLinecap="round"
+                        />
+                      </g>
+                    </svg>
+                  </dt>
+                  <p className="text-white ml-2">
+                    Adresse: <br />
+                    Suite 110 <br />
+                    150 Midsummer Boulevard <br />
+                    Milton Keynes <br />
+                    MK9 1FD
+                    <br />
+                    England{" "}
+                  </p>
+                  <dt className="flex-none">
+                    <span className="sr-only">Adresse</span>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      xmlnsXlink="http://www.w3.org/1999/xlink"
+                      version="1.1"
+                      width="11"
+                      height="12"
+                      viewBox="0 0 256 256"
+                      xmlSpace="preserve"
+                      className="h-11 w-12 text-white bg-[#25aae1] p-4 rounded-full"
+                    >
+                      <defs></defs>
+                      <g
                         style={{
                           stroke: "none",
-                          strokeWidth: "1",
+                          strokeWidth: "0",
                           strokeDasharray: "none",
                           strokeLinecap: "butt",
                           strokeLinejoin: "miter",
                           strokeMiterlimit: "10",
-                          fill: "rgb(255,255,255)", // White fill color
+                          fill: "none",
                           fillRule: "nonzero",
                           opacity: "1",
                         }}
-                        transform=" matrix(1 0 0 1 0 0) "
-                        strokeLinecap="round"
-                      />
-                    </g>
-                  </svg>
-                </dt>
-                <p className="text-white">
-                  Suite 110 <br />
-                  150 Midsummer Boulevard <br />
-                  Milton Keynes <br />
-                  MK9 1FD
-                  <br />
-                  England{" "}
-                </p>
+                        transform="translate(1.4065934065934016 1.4065934065934016) scale(2.81 2.81)"
+                      >
+                        <path
+                          d="M 45 90 c -1.415 0 -2.725 -0.748 -3.444 -1.966 l -4.385 -7.417 C 28.167 65.396 19.664 51.02 16.759 45.189 c -2.112 -4.331 -3.175 -8.955 -3.175 -13.773 C 13.584 14.093 27.677 0 45 0 c 17.323 0 31.416 14.093 31.416 31.416 c 0 4.815 -1.063 9.438 -3.157 13.741 c -0.025 0.052 -0.053 0.104 -0.08 0.155 c -2.961 5.909 -11.41 20.193 -20.353 35.309 l -4.382 7.413 C 47.725 89.252 46.415 90 45 90 z"
+                          style={{
+                            stroke: "none",
+                            strokeWidth: "1",
+                            strokeDasharray: "none",
+                            strokeLinecap: "butt",
+                            strokeLinejoin: "miter",
+                            strokeMiterlimit: "10",
+                            fill: "rgb(255,255,255)", // White fill color
+                            fillRule: "nonzero",
+                            opacity: "1",
+                          }}
+                          transform=" matrix(1 0 0 1 0 0) "
+                          strokeLinecap="round"
+                        />
+                        <path
+                          d="M 45 45.678 c -8.474 0 -15.369 -6.894 -15.369 -15.368 S 36.526 14.941 45 14.941 c 8.474 0 15.368 6.895 15.368 15.369 S 53.474 45.678 45 45.678 z"
+                          style={{
+                            stroke: "none",
+                            strokeWidth: "1",
+                            strokeDasharray: "none",
+                            strokeLinecap: "butt",
+                            strokeLinejoin: "miter",
+                            strokeMiterlimit: "10",
+                            fill: "rgb(255,255,255)", // White fill color
+                            fillRule: "nonzero",
+                            opacity: "1",
+                          }}
+                          transform=" matrix(1 0 0 1 0 0) "
+                          strokeLinecap="round"
+                        />
+                      </g>
+                    </svg>
+                  </dt>
+                  <p className="text-white ml-2">
+                    Coordonnées satellitaires <br />
+                    C/O Bloc A4 N 85
+                    <br />
+                    Cité Alqods <br />
+                    Agadir, Morocco <br />
+                  </p>
+                </div>
               </div>
             </dl>
             <p className="text-white mt-8 leading-6">
